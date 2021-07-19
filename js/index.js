eea187169424a07aca442357ddbf5700f719b892
Ryan Furrer <rfurrertech@gmail.com>
2021-07-19 19:21:22 -0400

    Added Cloudinary CDN to website
    - Changed image src to cloudinary
    - Made .full-width-container less wide
    - Fixed alt text and titles on "Photos" page

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var cloudinary = require("cloudinary-core"); // If your code is for ES5
+// import {Cloudinary} from “../node_modules/cloudinary-core/cloudinary-core”;    // If your code is for ES6 or higher
+var cl = new cloudinary.Cloudinary({cloud_name: "ryan-furrer", secure: true});
